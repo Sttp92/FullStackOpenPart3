9465c7450ce8080745a699870d665b26d09d74b5
Sttp92 <esteban.estay@ug.uchile.cl>
2024-11-13 20:31:17 -0300

    Add scripts for dist

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqInfo'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     { 
